d8a5e67d2fc2ac4490946ec22acfa40a61455109
watrgoat <oliveradams@iceeducate.org>
2023-03-15 14:39:20 -0400

    added multiprocessing

--- test_get.py
+++ test_get.py
@@ -1,6 +1,7 @@
 import requests
 import json
-import time
+from multiprocessing import Pool
+
 
 def write(urls: set):
     # read past urls into set
@@ -22,19 +23,24 @@ def write(urls: set):
 
 hdrs = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15', 'Referer':'http://www.google.com/', 'Accept-Language':'en-gb'}
 
-for num in range(300, 1500):
+def main(num):
     r = requests.get(f"https://www.poetryfoundation.org/ajax/poems?page={num}&sort_by=recently_added", headers=hdrs)
 
     if r.status_code != 200:
         print("response not given")
-        continue
+        return
 
     data = json.loads(r.text)
-
+    # possibly get snippet data for input, title, author?, tags, type
     urls = set()
     for d in data['Entries']:
         urls.add(d.get('link'))
 
     write(urls)
     print(f'Done on url #: {num}')
+    return
 
+if __name__ == '__main__':
+    numbas = list(range(1, 5))
+    with Pool(3) as p:
+        p.map(main, numbas)
