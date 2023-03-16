f99771b5e5d378e50b1dd39fe13d808cf2c0fb18
watrgoat <oliveradams@iceeducate.org>
2023-03-16 13:52:36 -0400

    poem scraping is up and working

--- scrape_poem.py
+++ scrape_poem.py
@@ -12,29 +12,40 @@ from multiprocessing import cpu_count
 from multiprocessing import Pool
 import time
 
+hdrs = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15', 'Referer':'http://www.google.com/', 'Accept-Language':'en-gb'}
 
 def scrape_poem(url):
-    r = requests.get(url)
+    # need to check if poem is an image
+    r = requests.get(url, headers=hdrs, allow_redirects=False)
+    if r.status_code != 200:
+        return
     
     soup = BeautifulSoup(r.text, features='html.parser')
 
-    result = soup.find_all('div', {'style':'text-indent: -1em; padding-left: 1em;'})
+    # get the poem text
     poem = ''
-    for div in result:
+    for div in soup.find_all('div', {'style':'text-indent: -1em; padding-left: 1em;'}):
         poem += div.text.strip()+'\n'
 
+    # get the poem tags
     ugly_tags = soup.find_all('a', href=re.compile('https://www.poetryfoundation.org/poems/browse#topics=[0-9]+'))
     tags = []
-
     for U_tag in ugly_tags:
         tags.append(U_tag.text)
+    
+    # get the poem title
+    title = soup.find('h3', {'class': 'c-hdgSans c-hdgSans_5 c-mix-hdgSans_blocked'}).text
 
-    return tags
+    author = soup.find('span', {'class':'c-txt c-txt_attribution'}).text.strip()[3:]
+
+    return title, author, poem.strip(), tags
 
 
 def get_urls():
-    with open('urls.txt', 'r', encoding='utf-8') as f:
-        urls = f.readlines()[0].split(' ')
+    with open('urls.txt', mode='r', encoding='utf-8') as f:
+        urls = f.read()
+
+    urls = urls.split('\n')
     
     return urls
 
@@ -48,16 +59,14 @@ def save_data(data):
 
     pass
 
-
 def main():
     urls = get_urls()
-    test_url = 'https://www.poetryfoundation.org/poetrymagazine/poems/159609/11-violence-anglo-linguistic'
+    urls = urls[:10]
     print(len(urls))
-    
     # with Pool(cpu_count()) as p:
     #     results = p.map(scrape_poem, urls)
-    
-    print(scrape_poem(test_url))
+    for url in urls:
+        print(scrape_poem(url))
 
 
 if __name__ == '__main__':
